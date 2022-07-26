a499cb0630fb6b5724841ba7ce73be93ec82af7a
baggette <play.email200620062006@gmail.com>
2022-07-26 18:30:43 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,7 @@
+print("stuff" + " stuff")
+name = input("beans\n")
+print(name+" beans")
+menu = "beans\n beans2 \n beans3"
+print(menu)
+bean = input("what bean\n")
+print("you can't have " + bean + ", its mine")
